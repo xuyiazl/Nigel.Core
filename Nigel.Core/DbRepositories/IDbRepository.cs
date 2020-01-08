815a04db9770321d7fba45e0a5234e536e24d042
xuyi <3624091@qq.com>
2020-01-08 17:42:41 +0800

    add linq

---
+++ Nigel.Core/DbRepositories/IDbRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using Nigel.Core.Collection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nigel.Core.DbRepositories
+{
+
+    public interface IDbRepository<TEntity> : IDbQueryRepository<TEntity>, IDbChangeRepository<TEntity>, IDbSaveRepository<TEntity> where TEntity : class
+    {
+
+    }
+}
---
+++ Nigel.Core/DbRepositories/IDbSaveRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using Nigel.Core.Collection;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nigel.Core.DbRepositories
+{
+    public interface IDbSaveRepository<TEntity> where TEntity : class
+    {
+        DbContext Context { get; }
+        DbSet<TEntity> Table { get; }
+        DatabaseFacade Database { get; }
+        bool IsNoTracking { get; set; }
+        bool Save();
+        Task<bool> SaveAsync();
+        Task<bool> SaveAsync(CancellationToken cancellationToken = default);
+        IDbContextTransaction BeginTransaction();
+        void CommitTransaction();
+        void RollbackTransaction();
+    }
+}
