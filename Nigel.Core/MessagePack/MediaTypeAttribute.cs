20a1594d5ed05df9defd0edbfdfc70e6bec123ec
Nigel <Nigel@DESKTOP-VVIB5CF>
2020-04-20 07:57:40 +0800

    修改MessagePack标签默认参数以及Result

---
+++ Nigel.Core/MessagePack/MediaTypeAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nigel.Core.MessagePack
+{
+    public class MediaTypeAttribute : ProducesAttribute
+    {
+        public MediaTypeAttribute() : base("application/x-msgpack", "application/json")
+        {
+
+        }
+
+        public MediaTypeAttribute(string contentType, params string[] additionalContentTypes) : base(contentType, additionalContentTypes)
+        {
+
+        }
+    }
+}
