97104c871fee7a90ff08faceb886f370c8d8f2c6
Nigel <Nigel@DESKTOP-QJBMC2S>
2020-05-11 11:40:54 +0800

    client

--- Nigel.Core/HttpFactory/HttpAuthorizationHeaderExtensions.cs
+++ Nigel.Core/HttpFactory/HttpAuthorizationHeaderExtensions.cs
@@ -8,21 +8,29 @@ namespace Nigel.Core.HttpFactory
 {
     public static class HttpAuthorizationHeaderExtensions
     {
-        public static void SetToken(this HttpClient client, string scheme, string token)
+        public static HttpClient SetToken(this HttpClient client, string scheme, string token)
         {
             client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(scheme, token);
+
+            return client;
         }
-        public static void SetBearerToken(this HttpClient client, string token)
+        public static HttpClient SetBearerToken(this HttpClient client, string token)
         {
             client.SetToken("Bearer", token);
+
+            return client;
         }
-        public static void SetToken(this HttpRequestMessage request, string scheme, string token)
+        public static HttpRequestMessage SetToken(this HttpRequestMessage request, string scheme, string token)
         {
             request.Headers.Authorization = new AuthenticationHeaderValue(scheme, token);
+
+            return request;
         }
-        public static void SetBearerToken(this HttpRequestMessage request, string token)
+        public static HttpRequestMessage SetBearerToken(this HttpRequestMessage request, string token)
         {
             request.SetToken("Bearer", token);
+
+            return request;
         }
     }
 }
